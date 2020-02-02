e49afdb0cd4529300ae5bbe8738a447125b370cd
firasShawa <firas.shawa22@gmail.com>
2020-02-03 01:52:30 +0300

    refactor jsonReader

--- app/src/main/java/com/firasshawa/pageviewer/MainActivity.java
+++ app/src/main/java/com/firasshawa/pageviewer/MainActivity.java
@@ -4,40 +4,80 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.JsonReader;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 import com.google.gson.Gson;
 
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringReader;
-import java.nio.Buffer;
-import java.sql.SQLOutput;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView QuotesListView;
+    ArrayList<String> StringQuotes;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        QuotesListView = findViewById(R.id.QuotesListView);
+
+        FillListview();
+
+    }
+
+    public void FillListview(){
+        StringQuotes = new ArrayList<>();
+
+        String QuotesString = ReadJson();
+        Quote[] Quotes = new Gson().fromJson(QuotesString,Quote[].class);
+
+        for (Quote singleQuote : Quotes) {
+            StringQuotes.add(singleQuote.getText());
+        }
+
+        ArrayAdapter<String> quoteArrayAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,StringQuotes);
+        QuotesListView.setAdapter(quoteArrayAdapter);
+    }
+
+    public String ReadJson(){
+        InputStream quotesStream;
+        int size;
+        byte[] buffer;
+        String JsonString;
         try {
-            InputStream qoutesStream = getAssets().open("qoutes.json");
-            int size = qoutesStream.available();
-            byte[] buffer = new byte[size];
-            qoutesStream.read(buffer);
-            qoutesStream.close();
-            String q = new String(buffer,"UTF-8");
-            JsonReader jsonReader = new JsonReader(new StringReader(q));
-            Gson gsonQoutes = new Gson();
-            Quote[] Quotes = gsonQoutes.fromJson(q,Quote[].class);
-            System.out.println();
-            for (Quote singleqoute:Quotes) {
-                System.out.println("=> "+ singleqoute.getText());
-            }
+            //open the file from the assets local folder
+            quotesStream = getAssets().open("quotes.json");
+
+            //getting the size  of the file
+            size = quotesStream.available();
+
+            //buffering the file into bytes
+            buffer = new byte[size];
+
+            //read the file and close it
+            quotesStream.read(buffer);
+            quotesStream.close();
+
+            //parsing the
+            JsonString = new String(buffer, "UTF-8");
+
+
+            return JsonString;
 
         } catch (IOException e) {
             e.printStackTrace();
+            System.out.println(e.getMessage());
+            JsonString = "[\n" +
+                    "    {\"text\" : \"Error!\"}\n" +
+                    "]";
+
+            return JsonString;
         }
     }
+
 }
